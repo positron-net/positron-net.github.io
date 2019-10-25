0423e1bc293ed4632d25f9f9500f93495ee04268
Boardens <31590269+boardens@users.noreply.github.com>
2019-10-25 21:26:24 +0200

    Add javascript ressources

---
+++ js/random-drag.js
@@ -0,0 +1,35 @@
+function getRandomInt (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$.fn.randomOrder = function(animate) {
+	this.each(function() {
+		var image = $(this);
+		
+		// Viewport Dimensions
+		var vpHeight = $(window).height();
+		var vpWidth = $(window).width();
+		
+		// Image Position
+		var xPos = getRandomInt(0, vpWidth - image.width());
+		var yPos = getRandomInt(0, vpHeight - image.height());
+		var zIndex = getRandomInt(0,13);
+		
+		// Animation Duration
+		if(animate) var dur = 500;
+		else var dur = 0;
+		
+		image.animate({left: xPos, top: yPos, 'z-index': zIndex}, dur);
+
+	});
+};
+
+// Setup
+$('.grab').randomOrder(false);
+$('.grab').draggable({stack: ".grab"});
+
+/*
+// Change after 10 Seconds
+window.setInterval(function(){
+	$('img.thumb').randomOrder(true);
+}, 10000);*/
